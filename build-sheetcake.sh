b3ee372bb7c25741714dbe4a23a10e3f78c15d51
Brian Taylor Vann <brian.t.vann@gmail.com>
2021-04-04 23:13:34 -0700

    feat(sheetcake): add attributes

--- build-sheetcake.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-deno bundle --config ./tsconfig.json ./src/sheetcake.ts ./esmodules/sheetcake.js 
-deno bundle --config ./tsconfig.json ./src/sheetcake.test.ts ./esmodules/sheetcake.test.js 
